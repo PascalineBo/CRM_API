22d3c7a1a9edc626a83453226ba4689016121b85
Marguerite Effren <pascaline_vigouroux@hotmail.com>
2022-12-12 14:01:23 +0100

    changement de formulaire User dans interface Admin Django

--- Epic/authentication/admin.py
+++ Epic/authentication/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 from .models import User
 
-admin.site.register(User, UserAdmin)
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('position','phone')}),
+    )
+
+admin.site.register(User, MyUserAdmin)
